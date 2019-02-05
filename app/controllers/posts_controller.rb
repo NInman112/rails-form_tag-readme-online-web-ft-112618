9d3039dca08898bfbe1a150b1da6f7b7a6d88a0f
Nick Inman <nick@theoatmeal.com>
2019-02-05 18:47:53 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,6 +5,11 @@ class PostsController < ApplicationController
 
   def new
 
-  end 
+  end
+
+  def create
+  Post.create(title: params[:post][:title], description: params[:post][:description])
+  redirect_to posts_path
+end
 
 end
